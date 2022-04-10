d4240d35c6fcee0ab4fcd0757261e0d73bd8ec57
connot <connor@hugapp.co.kr>
2022-04-10 19:10:39 +0900

    server DB,AWS 연동
    server DB,AWS 연동

--- Server/App/src/main/java/com/example/app/AppApplication.java
+++ Server/App/src/main/java/com/example/app/AppApplication.java
@@ -2,12 +2,19 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AppApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        new SpringApplicationBuilder(AppApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
