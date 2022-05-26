22fbd72a0101ce3b3cbb897a48a1751ef946d0f0
Yamewrong <90810696+Yamewrong@users.noreply.github.com>
2022-05-26 23:24:20 +0900

    Toast Message

--- app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/RegisterActivity.kt
+++ app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/RegisterActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.capstone.capstonenft.R
 import com.capstone.capstonenft.base.BaseActivity
 import com.capstone.capstonenft.databinding.ActivityLoginBinding
 import com.capstone.capstonenft.databinding.ActivityRegisterBinding
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : BaseActivity() {
     val TAG: String = "RegisterActivity"
@@ -40,8 +42,9 @@ class RegisterActivity : BaseActivity() {
             R.id.ar_et_pwre -> {
 
             }
-            R.id.ar_et_rg->{
-
+            R.id.ar_et_rg-> {
+                if (ar_et_pw != ar_et_pwre)
+                    Toast.makeText(this@RegisterActivity,"비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
             }
         }
     }
