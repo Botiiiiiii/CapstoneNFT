c071555e1f88c04666b0d0d956ca0c2168c0fded
connot <connor@hugapp.co.kr>
2022-05-26 22:46:18 +0900

    login

--- app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/LoginActivity.kt
+++ app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/LoginActivity.kt
@@ -3,29 +3,25 @@ package com.capstone.capstonenft.ui.activity.login
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.capstone.capstonenft.R
+import com.capstone.capstonenft.base.BaseActivity
 import com.capstone.capstonenft.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
     val TAG: String = "LoginActivity"
     lateinit var mBinding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-//        mBinding.alLlLogin.setOnClickListener {
-//            val uri = "intent://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key=0b0ee0ad-62b3-4146-980b-531b3201265d#Intent;scheme=kakaotalk;package=com.kakao.talk;end"
-//            val intent = Intent.parseUri(uri, Intent.URI_INTENT_SCHEME);
-//            val existPackage = packageManager.getLaunchIntentForPackage(intent.getPackage()!!)
-//            if (existPackage != null) {
-//                startActivity(intent)
-//            } else {
-//                var marketIntent = Intent(Intent.ACTION_VIEW)
-//                marketIntent.setData(Uri.parse("market://details?id=com.kakao.talk"))
-//                startActivity(marketIntent)
-//            }
-//        }
+    }
+
+    fun onClick(v:View){
+        when(v.id){
+
+        }
     }
 
 }
