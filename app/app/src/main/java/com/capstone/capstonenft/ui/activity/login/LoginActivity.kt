981b2f4ee97b6600a82ad55d40967c778f92b4c0
connot <connor@hugapp.co.kr>
2022-05-26 23:00:26 +0900

    login

--- app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/LoginActivity.kt
+++ app/app/src/main/java/com/capstone/capstonenft/ui/activity/login/LoginActivity.kt
@@ -4,22 +4,42 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.capstone.capstonenft.R
 import com.capstone.capstonenft.base.BaseActivity
 import com.capstone.capstonenft.databinding.ActivityLoginBinding
+import com.capstone.capstonenft.viewmodel.login.LoginViewModel
 
 class LoginActivity : BaseActivity() {
     val TAG: String = "LoginActivity"
     lateinit var mBinding: ActivityLoginBinding
+    val mViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        mBinding.listener = this
+
+        mBinding.alEtId.setOnFocusChangeListener { _, b ->
+            mBinding.alLlId.isSelected = b
+        }
+        mBinding.alEtPw.setOnFocusChangeListener { _, b ->
+            mBinding.alLlPw.isSelected = b
+        }
+
     }
 
     fun onClick(v:View){
         when(v.id){
+            R.id.al_tv_login -> {
+
+            }
+
+            R.id.al_tv_register -> {
+
+            }
 
         }
     }
---
+++ app/app/src/main/java/com/capstone/capstonenft/viewmodel/login/LoginViewModel.kt
@@ -0,0 +1,15 @@
+package com.capstone.capstonenft.viewmodel.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    fun login(id:String, pw: String){
+//        {
+//            "name" : "nick_name",
+//            "pw" : "password",
+//            "fcm" : "{fcm token}"
+//        }
+
+    }
+}
