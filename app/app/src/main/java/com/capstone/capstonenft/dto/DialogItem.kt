2b7fbf0566dd7ccf03113aca1ca5da603b889d15
connot <connor@hugapp.co.kr>
2022-06-05 22:26:09 +0900

    공통 dialog

---
+++ app/app/src/main/java/com/capstone/capstonenft/dto/DialogItem.kt
@@ -0,0 +1,8 @@
+package com.capstone.capstonenft.dto
+
+data class DialogItem(
+    val okBtnName: String,
+    val cancelBtnName:String,
+    val title:String,
+    val content:String
+)
---
+++ app/app/src/main/java/com/capstone/capstonenft/ui/dialog/CommonDialog.kt
@@ -0,0 +1,18 @@
+package com.capstone.capstonenft.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class CommonDialog: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
